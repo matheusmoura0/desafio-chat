f0653e556afd86bccb5d8ab6632ccb4ce2d5e186
Matheus <matheusmoura2110@gmail.com>
2026-01-28 16:05:15 -0300

    adds sender and receiver id in serializer

--- chat_api/app/serializers/message_serializer.rb
+++ chat_api/app/serializers/message_serializer.rb
@@ -1,9 +1,9 @@
 class MessageSerializer
   include JSONAPI::Serializer
-  attributes :content, :created_at
+  attributes :content, :created_at, :sender_id, :receiver_id
 
-  belongs_to :sender, record_type: :user, serializer: :UserSerializer
-  belongs_to :receiver, record_type: :user, serializer: :UserSerializer
+  belongs_to :sender, record_type: :user, serializer: UserSerializer
+  belongs_to :receiver, record_type: :user, serializer: UserSerializer
 
   attribute :file_url do |object|
     if object.file.attached?
