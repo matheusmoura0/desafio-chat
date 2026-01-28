ce62e5b17ee67e800b081f77a9d5da06176d37b5
Matheus <matheusmoura2110@gmail.com>
2026-01-28 14:59:25 -0300

    creates message serializer

---
+++ chat_api/app/serializers/message_serializer.rb
@@ -0,0 +1,15 @@
+class MessageSerializer
+  include JSONAPI::Serializer
+  attributes :content, :created_at
+
+  belongs_to :sender, record_type: :user, serializer: :UserSerializer
+  belongs_to :receiver, record_type: :user, serializer: :UserSerializer
+
+  attribute :file_url do |object|
+    if object.file.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(object.file, only_path: true)
+    else
+      nil
+    end
+  end
+end
