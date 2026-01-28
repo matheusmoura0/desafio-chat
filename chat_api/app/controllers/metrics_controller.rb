2db6ec73a021f1cf1988d5ec68f2809498003ad2
Matheus <matheusmoura2110@gmail.com>
2026-01-28 16:34:08 -0300

    adds metrics serializer to controller

--- chat_api/app/controllers/metrics_controller.rb
+++ chat_api/app/controllers/metrics_controller.rb
@@ -2,10 +2,7 @@ class MetricsController < ApplicationController
   before_action :authorize_request 
 
   def index
-    render json: {
-      total_users: User.count,
-      total_messages: Message.count,
-      active_users_24h: User.where('updated_at > ?', 24.hours.ago).count # Exemplo
-    }
+    metrics = Metrics.new
+    render json: MetricsSerializer.new(metrics).serializable_hash
   end
 end
