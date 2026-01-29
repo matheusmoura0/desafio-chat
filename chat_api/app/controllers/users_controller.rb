14888d439b6600742d7cfa70bc99144b151ed150
Matheus <matheusmoura2110@gmail.com>
2026-01-29 14:43:11 -0300

    changes api to show the users in main screen

--- chat_api/app/controllers/users_controller.rb
+++ chat_api/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
     def index
         users = User.all.order(created_at: :desc)
-        render json: users, each_serializer: UserSerializer
+        render json: UserSerializer.new(users).serializable_hash
     end
 end
